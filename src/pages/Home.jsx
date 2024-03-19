f63642b7620c69adf482f47dea2a3c030a1c3c41
Bipin <bk3284488@gmail.com>
2024-03-19 09:50:40 +0530

    initial file setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="main-container">
+      <div className="heading">
+        <h1>Sudoku Game</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
